089714adf7da9944659a983d31ffb3b5580d5e63
Ashur Cabrera <hello@cabreramade.co>
2016-04-05 07:35:53 -0700

    improving init script

--- init.sh
+++ init.sh
@@ -1,3 +1,18 @@
+# A self-destructing script to quickly set up the new repository
+
+## Remove Rudy repository details
 git remote rm origin
 git checkout --orphan master
+
+## README
+if [ -f README.md ]; then
+	rm README.md
+fi
+
+echo "# " >> README.md
+
+## Delete this file
+rm init.sh
+
+## Unstage all files
 git rm -r --cached .
